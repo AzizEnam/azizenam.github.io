04de9b15685e42666c09a045389fbcead2fcb48f
SyedAzizEnam <azizenam@gmail.com>
2026-06-15 00:01:33 +0000

    Deploying to gh-pages from @ AzizEnam/azizenam.github.io@de89a3f3f7e792ca2f98d6f1aa314037f378e709 🚀

---
+++ _app/immutable/entry/_page.svelte.29b29ed8.js
@@ -0,0 +1,17 @@
+import{S as Z,i as ee,s as te,y as ae,a as U,k as c,q as a,z as se,c as V,l as m,m as u,r as s,h as n,n as o,A as ne,b as W,D as e,F as oe,g as re,d as ie,B as le}from"../chunks/index.963ae9d8.js";import{S as ce}from"../chunks/Seo.768f147b.js";function me(X){let p,z,r,h,I,k,S,$,x,i,P,v,q,C,_,B,N,b,Y,D,L,f,M,g,T,F,y,G,H,E;return p=new ce({props:{title:"Aziz Enam",description:"A builder in robotics and computer vision based in New York."}}),{c(){ae(p.$$.fragment),z=U(),r=c("div"),h=c("p"),I=a("I'm a "),k=c("em"),S=a("builder"),$=a(" in robotics and computer vision based in New York."),x=U(),i=c("p"),P=a(`I'm currently a founding member of technical staff at
+    `),v=c("a"),q=a("Alquist"),C=a(` working
+    on vision-based autonomous navigation. Before that, I spent nearly five
+    years at `),_=c("a"),B=a("Cruise"),N=a(` as a
+    senior applied scientist on the localization, calibration, and mapping team,
+    and earlier built computer-vision systems for Postmates' Serve delivery
+    robots. I'm named on several
+    `),b=c("a"),Y=a("patents"),D=a(" in autonomous-vehicle sensor calibration."),L=U(),f=c("p"),M=a("Take a look at my "),g=c("a"),T=a("resume"),F=a(`, or reach me at
+    `),y=c("a"),G=a("azizenam@gmail.com"),H=a("."),this.h()},l(t){se(p.$$.fragment,t),z=V(t),r=m(t,"DIV",{class:!0});var l=u(r);h=m(l,"P",{});var A=u(h);I=s(A,"I'm a "),k=m(A,"EM",{class:!0});var j=u(k);S=s(j,"builder"),j.forEach(n),$=s(A," in robotics and computer vision based in New York."),A.forEach(n),x=V(l),i=m(l,"P",{});var d=u(i);P=s(d,`I'm currently a founding member of technical staff at
+    `),v=m(d,"A",{class:!0,href:!0});var J=u(v);q=s(J,"Alquist"),J.forEach(n),C=s(d,` working
+    on vision-based autonomous navigation. Before that, I spent nearly five
+    years at `),_=m(d,"A",{class:!0,href:!0});var K=u(_);B=s(K,"Cruise"),K.forEach(n),N=s(d,` as a
+    senior applied scientist on the localization, calibration, and mapping team,
+    and earlier built computer-vision systems for Postmates' Serve delivery
+    robots. I'm named on several
+    `),b=m(d,"A",{class:!0,href:!0});var O=u(b);Y=s(O,"patents"),O.forEach(n),D=s(d," in autonomous-vehicle sensor calibration."),d.forEach(n),L=V(l),f=m(l,"P",{});var w=u(f);M=s(w,"Take a look at my "),g=m(w,"A",{class:!0,href:!0});var Q=u(g);T=s(Q,"resume"),Q.forEach(n),F=s(w,`, or reach me at
+    `),y=m(w,"A",{class:!0,href:!0});var R=u(y);G=s(R,"azizenam@gmail.com"),R.forEach(n),H=s(w,"."),w.forEach(n),l.forEach(n),this.h()},h(){o(k,"class","svelte-wmtdkw"),o(v,"class","link"),o(v,"href","https://www.alquistrobotics.com/"),o(_,"class","link"),o(_,"href","https://www.getcruise.com/"),o(b,"class","link"),o(b,"href","https://patents.google.com/?inventor=Syed+Aziz+Enam&assignee=GM+Cruise+Holdings+LLC.&country=US&num=25"),o(g,"class","link"),o(g,"href","/resume"),o(y,"class","link"),o(y,"href","mailto:azizenam@gmail.com"),o(r,"class","layout-md text-lg space-y-5")},m(t,l){ne(p,t,l),W(t,z,l),W(t,r,l),e(r,h),e(h,I),e(h,k),e(k,S),e(h,$),e(r,x),e(r,i),e(i,P),e(i,v),e(v,q),e(i,C),e(i,_),e(_,B),e(i,N),e(i,b),e(b,Y),e(i,D),e(r,L),e(r,f),e(f,M),e(f,g),e(g,T),e(f,F),e(f,y),e(y,G),e(f,H),E=!0},p:oe,i(t){E||(re(p.$$.fragment,t),E=!0)},o(t){ie(p.$$.fragment,t),E=!1},d(t){le(p,t),t&&n(z),t&&n(r)}}}class fe extends Z{constructor(p){super(),ee(this,p,null,me,te,{})}}export{fe as default};
